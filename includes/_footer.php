92a04e249156273be57d57cb5832c3c28991f8ac
Nishikant Singh <nishikantsingh2004@gmail.com>
2024-08-01 18:12:29 +0530

    added alts

--- includes/_footer.php
+++ includes/_footer.php
@@ -45,8 +45,8 @@
                 Phone: 0240-2402383, 82, 81
             </p>
             <p>
-                <a href="https://www.facebook.com/profile.php?id=100063937994949&mibextid=ZbWKwL" alt="Facebook Profile" target="_blank">
-                    <img src="<?php echo $ROOTURL; ?>public/assets/facebookIcon.png" height="50" />
+                <a href="https://www.facebook.com/profile.php?id=100063937994949&mibextid=ZbWKwL" target="_blank">
+                    <img src="<?php echo $ROOTURL; ?>public/assets/facebookIcon.png" alt="Facebook Profile" height="50" />
                 </a>
             </p>
         </div>
