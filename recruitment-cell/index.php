bc49ec55f9718f22386ff4d97e87d844467ed939
Moh1tsingh <thakurmohitsingh2003@gmail.com>
2024-08-23 13:29:00 +0530

    add file in recruitment cell tab

--- recruitment-cell/index.php
+++ recruitment-cell/index.php
@@ -2,7 +2,7 @@
 
 $ROOT = "../"; 
 include($ROOT . "includes/_init.php");
-$CURRENTDIRURL = $ROOTURL . "";
+$CURRENTDIRURL = $ROOTURL . "recruitment-cell/";
 
 ?>
 
@@ -18,12 +18,27 @@ $CURRENTDIRURL = $ROOTURL . "";
     <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/global.css"></link>
     <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/_navbar.css"></link>
     <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/_footer.css"></link>
+    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/about-us/about-us.css"></link>
 </head>
 <body>
 
     <?php include($ROOT . "includes/_navbar.php"); ?>
 
-    <?php include($ROOT . "includes/_maintenance.php"); ?>
+    <div class="pageBanner">
+        <img src="<?php echo $ROOTURL ?>public/assets/pageBanner.jpg" loading="lazy" />
+        <h1>
+            RECRUITMENT CELL
+        </h1>
+    </div>
+    <div>
+        <div class="main">
+            <div class="certificate-container">
+                <ul class="unOrderedList">
+                    <li><a href="<?php echo $CURRENTDIRURL ?>assets/सहयोगी प्राध्यापक सामाजिक दंतशास्त्र करार पद्धतीने पदभरती जाहिरात.pdf">सहयोगी प्राध्यापक सामाजिक दंतशास्त्र करार पद्धतीने पदभरती जाहिरात</a></li>
+                </ul>
+            </div> 
+        </div>             
+    </div>
 
     <?php include($ROOT . "includes/_footer.php"); ?>
 
