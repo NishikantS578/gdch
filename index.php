3dcacf51f05dd4a1f2c77f217b11a4428db67d0b
Nishikant Singh <nishikantsingh2004@gmail.com>
2024-10-22 13:31:23 +0530

    changed deans message alignments

--- index.php
+++ index.php
@@ -162,21 +162,13 @@ $CURRENTDIRURL = $ROOTURL . "";
             <h2>
                 DEAN'S MESSAGE
             </h2>
-            <img style="border-radius:10px;" src="<?php echo $CURRENTDIRURL ?>assets/dean.jpg" alt="Dean's Photo" loading="lazy"></img>
             <div>
                 <p>
-                    I <strong>Dr. Maya S. Indurkar</strong> welcome you to this prestigious Govt. Dental College and Hospital Chh.Sambhajinagar(Aurangabad), only Govt college in Marathwada region. This institution was established in the year 1982 by Govt. of Maharashtra. It is my great pleasure to extend a warm welcome to each and everyone of you.
-                </p>
-                <p>
+                    <img style="border-radius:10px;" src="<?php echo $CURRENTDIRURL ?>assets/dean.jpg" alt="Dean's Photo" loading="lazy"></img>
+                    I <strong>Dr. Maya S. Indurkar</strong> welcome you to this prestigious Govt. Dental College and Hospital Chh. Sambhajinagar (Aurangabad), only Govt college in Marathwada region. This institution was established in the year 1982 by Govt. of Maharashtra. It is my great pleasure to extend a warm welcome to each and everyone of you.
                     At our institution, we are dedicated to fostering excellence in dental education, research, and patient care. Our distinguished faculty members are committed to provide you with the highest quality education. Our graduates leave here not only as skilled clinicians, but also as compassionate healthcare providers.
-                </p>
-                <p className="text-justify mb-4">
                     Our mission is clear: To empower our students with the knowledge, skills, and compassion needed to excel in the field of dentistry. We pride ourselves on providing a comprehensive education that blends cutting-edge technology with hands-on clinical experience.
-                </p>
-                <p className="text-justify mb-4">
                     We are also deeply committed to advancing dental research, pushing boundaries, and contributing to the evolving landscape of oral healthcare. Through our collaborative efforts, we aim to make lasting contributions to the field.
-                </p>
-                <p className="text-justify mb-4">
                     I encourage you to explore our website and learn more about the exciting opportunities and resources we offer. Whether you are a prospective student, a current member of our community, or a visitor interested in our work, I am confident you will find something inspiring here.
                 </p>
                 <h3 className="font-semibold">
