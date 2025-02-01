aeb23e79fb9b3b27d48e8f4c721898b5cafdf334
Mohitsingh Thakur <121103486+Moh1tsingh@users.noreply.github.com>
2025-02-01 17:24:45 +0530

    Add files via upload

---
+++ summit/index.php
@@ -0,0 +1,81 @@
+<?php
+
+$ROOT = "../";
+include($ROOT . "includes/_init.php");
+$CURRENTDIRURL = $ROOTURL . "summit/";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Governmentt Dental College and Hospital, Chhatrapati Sambhajinagar (GDCH) is one of the reputed Government Dental Colleges in Maharashtra, Affiliated with the Maharashtra University of Health Sciences, Nashik.">
+    <title>Government Dental College & Hospital Chhatrapati Sambhajinagar</title>
+    <link rel="canonical" href="https://gdchcsn.ac.in">
+    <link rel="icon" type="image/x-icon" href="<?php echo $ROOTURL ?>public/assets/gdclogo1.png">
+
+    <script src="<?php echo $ROOTURL ?>public/js/_navbar.js" defer></script>
+    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/global.css">
+    </link>
+    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/_navbar.css">
+    </link>
+    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/_footer.css">
+    </link>
+    </link>
+    
+    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/about-us/about-us.css"></link>
+</head>
+
+<body>
+
+    <?php include($ROOT . "includes/_navbar.php"); ?>
+
+    <div class="pageBanner">
+        <img src="<?php echo $ROOTURL ?>public/assets/pageBanner.jpg" loading="lazy" />
+        <h1>
+            DENTAL EDUCATION RESEARCH SUMMIT - II Transforming Dental Care Through Research & Innovation DMER - GDCH
+        </h1>
+    </div>
+    <div class="contentContainer">
+        <div class="certificate-container-summit">
+            <h2>
+                Registration
+            </h2>
+            <ul class="" style="text-align: center; list-style-type: none; padding: 0;">
+                <li>
+                    <a style="text-align: center; font-size: 1.4rem;" href="<?php echo $CURRENTDIRURL ?>assets/FINAL brochure  5th feb.pdf" target="_blank">
+                        DENTAL EDUCATION RESEARCH SUMMIT-II BROCHURE  (CLICK HERE)
+                    </a>
+                </li>
+                
+            </ul>
+            <div style="display: flex; align-items: center; gap: 2rem; padding: 1rem; flex-direction: column;">
+                <div style="display: flex; align-items: center; gap: 2rem; padding: 1rem; flex-direction: row; flex-wrap: wrap; justify-content: center;">
+                    <div style="display: flex; flex-direction: column; align-items: center; width: 100%; max-width: 400px;">
+                        <h3 style="margin-bottom: 1rem; color: var(--primary-color);"><strong>Scan and Pay</strong></h3>
+                        <img src="<?php echo $CURRENTDIRURL ?>assets/Screenshot 2025-02-01 164500.png" loading="lazy" style="width: 100%; height: auto; max-width: 300px;"/>
+                    </div>
+                    <div style="width: 100%; max-width: 400px;">
+                        <ul style="list-style-type: none; padding: 0; margin: 0;">
+                            <li style="margin-bottom: 1rem; "><strong>Registration fees:</strong> Rs 500 </li>
+                            <li style="margin-bottom: 0.5rem;"><strong>Bank account number:</strong> 62095810275</li>
+                            <li style="margin-bottom: 0.5rem;"><strong>IFSC CODE:</strong> SBIN0017554</li>
+                            <li style="margin-bottom: 0.5rem;"><strong>NAME:</strong> DEAN,GOVT DENTAL COLLEGE & HOSPITAL, CHHATRAPATI SAMBHAJINAGAR</li>
+                            <li style="margin-bottom: 0.5rem;">FILL THE BELOW GOOGLE FORM AND SEND SCREENSHOT OF PAYMENT DETAILS TO 8788122732</li>
+                            <a style="margin-bottom: 0.5rem;" href="https://docs.google.com/forms/d/e/1FAIpQLSej1ayrLDSQ1QzzwhdfqcpI9d3h-OmJlsNHlwN-ny_q5Pd7bw/viewform?usp=sf_link" target="_blank"><strong>CLICK HERE TO GO TO GOOGLE FORM:</strong></a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <?php include($ROOT . "includes/_footer.php"); ?>
+
+</body>
+
+</html>
